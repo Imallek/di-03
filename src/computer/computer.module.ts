aafadbf47f00bc1f1fbc9860de11af639b36b4fb
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-03-13 15:44:47 +0500

    connect different modules

--- src/computer/computer.module.ts
+++ src/computer/computer.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CpuModule } from 'src/cpu/cpu.module';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskModule } from 'src/disk/disk.module';
 import { ComputerController } from './computer.controller';
 
 @Module({
-  controllers: [ComputerController]
+  controllers: [ComputerController],
+  imports: [CpuModule, DiskModule]
 })
-export class ComputerModule {}
+export class ComputerModule { }
